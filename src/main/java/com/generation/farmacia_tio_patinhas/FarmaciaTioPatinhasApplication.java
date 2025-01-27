a1e70192d3f3ba199f27e12878aba05415c086aa
Lucas300 <ludaniel.sd@gmail.com>
2025-01-27 10:15:03 -0300

    Adicionando as configurações do application.properties

---
+++ src/main/java/com/generation/farmacia_tio_patinhas/FarmaciaTioPatinhasApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia_tio_patinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaTioPatinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaTioPatinhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmacia_tio_patinhas/FarmaciaTioPatinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia_tio_patinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaTioPatinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
